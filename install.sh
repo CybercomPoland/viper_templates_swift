4ca6c8bfef935cdf888c5a6232c7f1d5a9ac593e
Łukasz Majchrzak <lukasz@ukaszs-MacBook-Air-2.local>
2017-05-26 18:25:40 +0200

    minor changes

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
-folderName="ViperModule"
-
-installDirectory=~/Library/Developer/Xcode/Templates/Custom/"${folderName}"
+installDirectory=~/Library/Developer/Xcode/Templates/Custom/ViperModule
 
 echo "Templates will be installed to ${installDirectory}"
-
 if [ -d "${installDirectory}" ]; then
     rm -r "${installDirectory}"
 fi
 
 mkdir -p "${installDirectory}"
-
 cp -r *.xctemplate "${installDirectory}"
 
 echo "ViperModule successfully installed!"
