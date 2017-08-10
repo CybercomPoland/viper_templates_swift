e8824a23046021cb39e4052b3bc4e71989d3ee29
Andrzej Jacak <andrzej.jacak@cybercom.com>
2017-08-10 17:42:52 +0200

    Updated and fixed issues.

---
+++ VIPERTable.xctemplate/___FILEBASENAME___Module/DataManager/___FILEBASENAME___DataManagerProtocols.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+// Interactor to DataManager
+protocol ___VARIABLE_viperModuleName___DataManagerInput: class {
+
+}
+
+// DataManager to Interactor
+protocol ___VARIABLE_viperModuleName___DataManagerOutput: class {
+
+}
---
+++ VIPERTable.xctemplate/___FILEBASENAME___Module/Interactor/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+extension ___VARIABLE_viperModuleName___Interactor: ___VARIABLE_viperModuleName___InteractorInput {
+
+}
+
+extension ___VARIABLE_viperModuleName___Interactor: ___VARIABLE_viperModuleName___DataManagerOutput {
+
+}
